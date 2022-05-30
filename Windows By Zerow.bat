030ac7b265acf797685b2d756dd90a050f7f4054
Zerow <105360899+DevZerow@users.noreply.github.com>
2022-05-30 20:23:34 +0200

    Add files via upload

---
+++ Windows By Zerow.bat
@@ -0,0 +1,76 @@
+@echo off
+chcp 65001
+title Windows 10 activator By Zerow&cls&echo ************************************&echo Produits pris en charge:&echo - Windows 10 Home&echo - Windows 10 Professional&echo - Windows 10 Enterprise, Enterprise LTSB&echo - Windows 10 Education&echo.&echo.&echo ************************************ &echo Windows 10 activation...&set i=1
+echo ************************************ &echo Copyright (c) 2022 Zerow L'autorisation est accordée, gratuitement, à toute personne obtenant une copie de ce logiciel et des fichiers de documentation associés (le "Logiciel"), de traiter le Logiciel sans restriction, y compris, sans limitation, les droits d'utilisation, de copie, de modification, de fusion , publier, distribuer, sous-licencier et/ou vendre des copies du Logiciel, et permettre aux personnes à qui le Logiciel est fourni de le faire, sous réserve des conditions suivantes : L'avis de droit d'auteur ci-dessus et cet avis d'autorisation doivent être inclus dans toutes les copies ou parties substantielles du Logiciel. LE LOGICIEL EST FOURNI « EN L'ÉTAT », SANS GARANTIE D'AUCUNE SORTE, EXPLICITE OU IMPLICITE, Y COMPRIS, MAIS SANS S'Y LIMITER, LES GARANTIES DE QUALITÉ MARCHANDE, D'ADÉQUATION À UN USAGE PARTICULIER ET D'ABSENCE DE CONTREFAÇON. EN AUCUN CAS, LES AUTEURS OU LES DÉTENTEURS DU COPYRIGHT NE SERONT RESPONSABLES DE TOUTE RÉCLAMATION, DOMMAGE OU AUTRE RESPONSABILITÉ, QUE CE SOIT DANS UNE ACTION CONTRACTUELLE, DÉLICTUELLE OU AUTRE, DÉCOULANT DE, DE OU EN RELATION AVEC LE LOGICIEL OU L'UTILISATION OU D'AUTRES TRANSACTIONS DANS LE LOGICIEL.
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ >nul
+echo ************************************ &echo.&echo.&set i=1
+:server
+if %i%==1 set KMS_Sev=kms.shuax.com
+if %i%==2 set KMS_Sev=NextLevel.uk.to
+if %i%==3 set KMS_Sev=GuangPeng.uk.to
+if %i%==4 set KMS_Sev=AlwaysSmile.uk.to
+if %i%==5 set KMS_Sev=kms.chinancce.com
+if %i%==6 set KMS_Sev=kms.shuax.com
+if %i%==7 set KMS_Sev=kms.srv.crsoo.com
+if %i%==8 set KMS_Sev=cy2617.jios.org
+if %i%==9 set KMS_Sev=kms.digiboy.ir
+if %i%==10 set KMS_Sev=kms.cangshui.net
+if %i%==11 set KMS_Sev=kms.library.hk
+if %i%==12 set KMS_Sev=hq1.chinancce.com
+if %i%==13 set KMS_Sev=kms.loli.beer
+if %i%==14 set KMS_Sev=kms.v0v.bid
+if %i%==15 set KMS_Sev=54.223.212.31
+if %i%==16 set KMS_Sev=kms.jm33.me
+if %i%==17 set KMS_Sev=nb.shenqw.win
+if %i%==18 set KMS_Sev=kms.izetn.cn
+if %i%==19 set KMS_Sev=kms.cin.ink
+if %i%==20 set KMS_Sev=222.184.9.98
+if %i%==21 set KMS_Sev=kms.ijio.net
+if %i%==22 set KMS_Sev=fourdeltaone.net:1688
+if %i%==23 set KMS_Sev=kms.iaini.net
+if %i%==24 set KMS_Sev=kms.cnlic.com
+if %i%==25 set KMS_Sev=kms.51it.wang
+if %i%==26 set KMS_Sev=key.17108.com
+if %i%==27 set KMS_Sev=kms.chinancce.com
+if %i%==28 set KMS_Sev=kms.ddns.net
+if %i%==29 set KMS_Sev=windows.kms.app
+if %i%==30 set KMS_Sev=kms.ddz.red
+if %i%==31 set KMS_Sev=franklv.ddns.net
+if %i%==32 set KMS_Sev=kms.mogeko.me
+if %i%==33 set KMS_Sev=k.zpale.com
+if %i%==34 set KMS_Sev=amrice.top
+if %i%==35 set KMS_Sev=m.zpale.com
+if %i%==36 set KMS_Sev=mvg.zpale.com
+if %i%==37 set KMS_Sev=kms.shuax.com
+if %i%==38 set KMS_Sev=kensol263.imwork.net:1688
+if %i%==39 set KMS_Sev=xykz.f3322.org
+if %i%==40 set KMS_Sev=kms789.com
+if %i%==41 set KMS_Sev=dimanyakms.sytes.net:1688
+if %i%==42 set KMS_Sev=kms8.MSGuides.com
+if %i%==43 set KMS_Sev=kms.03k.org:1688
+if %i%==44 set KMS_Sev=kms.ymgblog.com
+if %i%==45 set KMS_Sev=kms.bige0.com
+if %i%==46 set KMS_Sev=kms9.MSGuides.com
+if %i%==47 set KMS_Sev=kms.cz9.cn
+if %i%==48 set KMS_Sev=kms.lolico.moe
+if %i%==49 set KMS_Sev=kms.ddddg.cn
+if %i%==50 set KMS_Sev=kms.zhuxiaole.org
+if %i%==51 set KMS_Sev=kms.moeclub.org
+if %i%==52 set KMS_Sev=kms.lotro.cc
+if %i%==53 set KMS_Sev=zh.us.to
+if %i%==54 set KMS_Sev=noair.strangled.net:1688
+if %i%==55 exit
+cscript //nologo c:\windows\system32\slmgr.vbs /skms %KMS_Sev% >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ato | find /i "successfully" && (echo.& echo ************************************ & echo. & choice /n /c YN /m "Voulez-vous redémarrer votre PC maintenant [Y,N]?" & if errorlevel 2 exit) || (echo La connexion au serveur a échoué ! Tentative de connexion à un autre... &echo Veuillez patienter... & echo. & echo. & set /a i+=1 & goto server)
+shutdown.exe /r /t 00
